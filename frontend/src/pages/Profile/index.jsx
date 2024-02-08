8a021630e0c80d332f4f5421332bb7b0132693ea
Timur Ramazanov <timurram007@gmail.com>
2024-02-08 01:54:52 +0100

    sign out from profile

--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -1,17 +1,14 @@
-import React, { useContext } from 'react'
-import { Button, Divider, Title } from '../../components'
-import { useTranslation } from 'react-i18next'
-import AuthContext from '../../contexts/AuthContext'
+import { Divider } from '../../components'
 import PersonalInfo from './PersonalInfo'
 import DeleteProfile from './DeleteProfile'
 import ChangePassword from './ChangePassword'
+import Logout from './Logout'
 
 const Profile = () => {
-    const { t } = useTranslation()
-    const { logoutUser } = useContext(AuthContext)
     return (
         <div className="container mx-auto flex flex-col gap-10 md:gap-16 pb-10 md:pb-16 mt-20">
-            {/* <Button action={logoutUser}>logout</Button> */}
+            <Logout />
+            <Divider horizontal />
             <PersonalInfo />
             <Divider horizontal />
             <ChangePassword />
