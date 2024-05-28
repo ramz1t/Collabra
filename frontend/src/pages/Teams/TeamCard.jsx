f020e1b2afee01e833e6bd5c2675cdf65c98b12e
Timur Ramazanov <timurram007@gmail.com>
2024-05-28 23:46:36 +0200

    new teams list

--- frontend/src/pages/Teams/TeamCard.jsx
+++ frontend/src/pages/Teams/TeamCard.jsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next'
 import TeamStats from './TeamStats.jsx'
 import { TeamImage } from '../../components/index.js'
 import useScreenSize from '../../hooks/useScreenSize.js'
+import cn from 'classnames'
 
 const membersMock = [
     {
@@ -35,21 +36,45 @@ const membersMock = [
     },
 ]
 
-const TeamCard = ({ team }) => {
+const TeamCard = ({ team, isList }) => {
     const { t } = useTranslation()
     const { isTablet } = useScreenSize()
     return (
-        <li>
+        <li
+            className={
+                isList &&
+                'md:odd:bg-gray-100 md:odd:dark:bg-slate-800 md:hover:bg-gray-200 md:hover:dark:bg-slate-700 md:rounded-lg'
+            }
+        >
             <NavLink
                 to={team.slug}
-                className="rounded-lg max-md:bg-gray-50 group-card hover:md:bg-gray-100 dark:hover:md:bg-slate-800 h-fit overflow-hidden dark:border-gray-700 hover:!border-accent hover:dark:!border-accent-dark transition-all duration-75 flex items-center gap-7 p-5"
+                className={cn(
+                    isTablet && isList
+                        ? 'w-full px-4 py-3'
+                        : 'rounded-lg max-md:bg-gray-50 group-card hover:md:bg-gray-100 dark:hover:md:bg-slate-800 p-5',
+                    'group-card h-fit overflow-hidden dark:border-gray-700 hover:!border-accent hover:dark:!border-accent-dark transition-all duration-75 flex items-center gap-7'
+                )}
             >
-                {isTablet && <TeamImage team={team} size="list" />}
-                <div className="">
-                    <p className="text-2xl font-bold line-clamp-1">
+                {isTablet && !isList && <TeamImage team={team} size="list" />}
+                <div
+                    className={
+                        isTablet &&
+                        isList &&
+                        'grid md:grid-cols-[1fr_2fr] xl:grid-cols-[1fr_1fr] gap-10 w-full items-center'
+                    }
+                >
+                    <p
+                        className={cn(
+                            isTablet && isList ? 'text-xl' : ' text-2xl',
+                            'font-bold line-clamp-1 whitespace-nowrap'
+                        )}
+                    >
                         {team.title}
                     </p>
-                    <TeamStats team={{ ...team, members: membersMock }} />
+                    <TeamStats
+                        team={{ ...team, members: membersMock }}
+                        isList={isList}
+                    />
                 </div>
             </NavLink>
         </li>
--- frontend/src/pages/Teams/TeamsList.jsx
+++ frontend/src/pages/Teams/TeamsList.jsx
@@ -1,39 +1,87 @@
 import React, { useEffect } from 'react'
 import TeamCard from './TeamCard'
 import { Button, Input, SearchBar } from '../../components/index.js'
-import { IoDuplicateOutline } from 'react-icons/io5'
+import {
+    IoDuplicateOutline,
+    IoReorderFourOutline,
+    IoGridOutline,
+} from 'react-icons/io5'
 import { useTranslation } from 'react-i18next'
 import useInput from '../../hooks/useInput.js'
 import { useTeams } from '../../api/team.js'
-import useDebounce from '../../hooks/useDebounce.js'
-import { motion, AnimatePresence } from 'framer-motion'
+import cn from 'classnames'
+import useLocalStorage from '../../hooks/useLocalStorage.js'
+import useScreenSize from '../../hooks/useScreenSize.js'
 
 const TeamsList = () => {
     const { t } = useTranslation()
     const search = useInput('', {}, 250)
     const { data } = useTeams({ search: search.value })
+    const { isTablet } = useScreenSize()
+    const [isList, setIsList] = useLocalStorage('displayTeamsInList', false)
 
     useEffect(() => {
         if (search.value === '') search.setValue(null)
     }, [search.value])
 
     return (
-        <div className="pt-5">
-            <div className="flex md:items-center gap-3 md:gap-5 flex-col-reverse md:flex-row items-start max-md:flex-wrap">
-                <Button to="create" style="primary" className="max-md:w-full">
+        <div>
+            <div className="flex py-5 items-end gap-5 flex-wrap">
+                <h1 className="font-bold text-3xl mr-auto">{t('teams')}</h1>
+                {isTablet && (
+                    <div className="flex gap-1 items-center">
+                        <p className="hidden md:block font-semibold">
+                            {t('view_as')}
+                        </p>
+                        <Button
+                            action={() => setIsList(false)}
+                            className={cn(
+                                !isList
+                                    ? 'bg-gray-200 hover:!opacity-100 dark:bg-slate-700'
+                                    : 'bg-gray-100 dark:bg-slate-800',
+                                'min-h-10 min-w-10 md:px-3 rounded-md ml-2'
+                            )}
+                        >
+                            <IoGridOutline />
+                            {isTablet && t('grid')}
+                        </Button>
+                        <Button
+                            action={() => setIsList(true)}
+                            className={cn(
+                                isList
+                                    ? 'bg-gray-200 hover:!opacity-100 dark:bg-slate-700'
+                                    : 'bg-gray-100 dark:bg-slate-800',
+                                'min-h-10 min-w-10 md:px-3 rounded-md'
+                            )}
+                        >
+                            <IoReorderFourOutline />
+                            {isTablet && t('list')}
+                        </Button>
+                    </div>
+                )}
+                <Button to="create" style="primary">
                     <IoDuplicateOutline />
                     {t('create_team')}
                 </Button>
+            </div>
+            <div className="flex md:items-center gap-3 md:gap-5 flex-col-reverse md:flex-row items-start max-md:flex-wrap">
                 <SearchBar
                     inputInstance={search}
-                    placeholder={t('search_team')}
+                    placeholder={`${t('search')}...`}
                 />
             </div>
 
             {data?.results && (
-                <ul className="grid gap-5 md:grid-cols-2 xl:grid-cols-3 pt-5">
+                <ul
+                    className={cn(
+                        isTablet && isList
+                            ? ''
+                            : 'grid gap-5 md:grid-cols-2 xl:grid-cols-3',
+                        'pt-5'
+                    )}
+                >
                     {data.results.map((team, key) => (
-                        <TeamCard key={key} team={team} />
+                        <TeamCard key={key} team={team} isList={isList} />
                     ))}
                 </ul>
             )}
