a79dbdedbd0054b0dd936354c9549eba31890d94
Timur Ramazanov <timurram007@gmail.com>
2024-06-19 15:44:55 +0200

    fix smoke test

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -9,7 +9,7 @@ export const useCheckConnection = () => {
         queryKey: ['connection'],
         queryFn: async (): Promise<boolean> => {
             const res = await api.get(`${prefix}/smoke`)
-            return res.data
+            return res.status === 200
         },
     })
 }
