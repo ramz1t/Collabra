f89d426ba8a8597bc48b2cbe334668fcdc8eac72
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2025-04-05 23:28:08 +0200

    refactor Board.tsx

--- frontend/src/pages/Team/Tasks/Tasks/Board/Board.tsx
+++ frontend/src/pages/Team/Tasks/Tasks/Board/Board.tsx
@@ -1,10 +1,13 @@
-import Column, { ColumnProps } from './Column'
-import { useTranslation } from 'react-i18next'
-import { useState } from 'react'
+import Column from './Column'
 import useLocalStorage from '../../../../../hooks/useLocalStorage'
-import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import { useCallback } from 'react'
 
-const initialColumns = [
+type ColumnType = {
+    status: string
+    canAdd: boolean
+}
+
+const initialColumns: ColumnType[] = [
     { status: 'to_do', canAdd: true },
     { status: 'in_progress', canAdd: true },
     { status: 'need_review', canAdd: false },
@@ -12,30 +15,37 @@ const initialColumns = [
 ]
 
 const Board = () => {
-    const [columns, setColumns] = useLocalStorage(
+    const [columns, setColumns] = useLocalStorage<ColumnType[]>(
         'taskboardColsPosition',
         initialColumns
     )
 
-    const moveColumn = (fromIndex: number, toIndex: number) => {
-        const newColumns = [...columns!]
-        const [movedColumn] = newColumns.splice(fromIndex, 1)
-        newColumns.splice(toIndex, 0, movedColumn)
-        setColumns(newColumns)
-    }
+    const moveColumn = useCallback(
+        (fromIndex: number, toIndex: number) => {
+            if (fromIndex === toIndex) return
+
+            setColumns((prevColumns) => {
+                if (!prevColumns) return []
+                const newColumns = [...prevColumns]
+                const [movedColumn] = newColumns.splice(fromIndex, 1)
+                newColumns.splice(toIndex, 0, movedColumn)
+                return newColumns
+            })
+        },
+        [setColumns]
+    )
 
     return (
         <div className="bg-gray-100 dark:bg-slate-900 grow grid grid-cols-[1fr_1fr_1fr_1fr] p-5 gap-5 items-start max-w-screen md:max-w-slot overflow-x-auto">
-            {columns &&
-                columns.map((col, key) => (
-                    <Column
-                        key={col.status}
-                        index={key}
-                        status={col.status}
-                        canAdd={col.canAdd}
-                        moveColumn={moveColumn}
-                    />
-                ))}
+            {columns?.map((col, key) => (
+                <Column
+                    key={col.status}
+                    index={key}
+                    status={col.status}
+                    canAdd={col.canAdd}
+                    moveColumn={moveColumn}
+                />
+            ))}
         </div>
     )
 }
