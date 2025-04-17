c4dbab6992925f4aa8efa9c18f42ebf8b5090869
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2025-04-17 13:09:54 +0200

    TeamTags components

---
+++ frontend/src/pages/Team/Settings/TeamTags/EditTagDialog.tsx
@@ -0,0 +1,81 @@
+import { Tag } from '../../../../types'
+import React, { useState } from 'react'
+import { IoPencil } from 'react-icons/io5'
+import { Button, DialogWindow, Input } from '../../../../components'
+import { useTranslation } from 'react-i18next'
+import useInput from '../../../../hooks/useInput'
+import cn from 'classnames'
+import { useDeleteTag, useEditTag } from '../../../../api/tags'
+import { useParams } from 'react-router-dom'
+
+const EditTagDialog = ({
+    tag,
+    isOpen,
+    setIsOpen,
+}: {
+    tag: Tag
+    isOpen: boolean
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+}) => {
+    const { t } = useTranslation()
+    const title = useInput(tag.title, { isEmpty: true })
+    const [color, setColor] = useState(tag.color)
+    const { teamSlug } = useParams()
+
+    const { mutate: deleteTag, isPending: isDeleting } = useDeleteTag(teamSlug!)
+    const { mutate: editTag, isPending: isEditing } = useEditTag(teamSlug!)
+
+    const isValid =
+        title.allValid &&
+        (title.value !== tag.title || color !== tag.color)
+
+    return (
+        <DialogWindow
+            icon={<IoPencil />}
+            title={t('edit_tag')}
+            isOpen={isOpen}
+            isLoading={isEditing}
+            onSuccess={() => editTag(tag.id)}
+            closeOnSuccess
+            disabled={!isValid}
+            close={() => setIsOpen(false)}
+            successButtonStyle="primary"
+            successButtonText={t('save')}
+            extraButtons={
+                <Button
+                    action={() => {
+                        deleteTag(tag.id, { onSuccess: () => setIsOpen(false) })
+                    }}
+                    style="destructive"
+                    isLoading={isDeleting}
+                >
+                    {t('delete')}
+                </Button>
+            }
+        >
+            <div className="pt-4 md:pt-2 grid gap-5">
+                <Input title={t('title')} instance={title} must />
+                <div className="flex flex-col gap-1">
+                    <label
+                        className='pl-1 after:content-["*"] after:text-red-500 dark:after:text-red-700 after:pl-0.5'
+                        htmlFor="#color"
+                    >
+                        {t('color')}
+                    </label>
+                    <input
+                        id="color"
+                        type="color"
+                        className="size-8 rounded-md border-none [&::-moz-color-swatch]:border-none"
+                        value={color}
+                        onChange={(e) => setColor(e.target.value)}
+                    />
+                </div>
+                <p className="font-semibold text-xs pt-2 text-gray-500">
+                    {t('delete_tag_warning')}
+                </p>
+            </div>
+        </DialogWindow>
+    )
+}
+
+export default EditTagDialog
---
+++ frontend/src/pages/Team/Settings/TeamTags/index.tsx
@@ -0,0 +1,96 @@
+import { useTranslation } from 'react-i18next'
+import {
+    Button,
+    LoadingState,
+    SettingsSection,
+    TaskTag,
+} from '../../../../components'
+import React, { useState } from 'react'
+import { hexToRGBA } from '../../../../utils'
+import { IoAdd } from 'react-icons/io5'
+import { useCreateTag, useTags } from '../../../../api/tags'
+import { useParams } from 'react-router-dom'
+
+const TeamTags = (): React.ReactElement => {
+    const { t } = useTranslation()
+    const { teamSlug } = useParams()
+    const [newTag, setNewTag] = useState('')
+    const initialColor = '#5b5b5b'
+    const [color, setColor] = useState(initialColor)
+    const { mutate: createTag } = useCreateTag(teamSlug!)
+    const { data: tags, isLoading } = useTags(teamSlug!)
+
+    return (
+        <SettingsSection
+            title={t('team_tags_head')}
+            description={t('team_tags_desc')}
+        >
+            <div>
+                <p className="font-semibold text-sm pb-2">
+                    {t('create_new_tag')}
+                </p>
+                <div
+                    className="w-fit bg-gray-100 rounded-full flex items-center pl-1 min-h-8"
+                    style={
+                        {
+                            backgroundColor: hexToRGBA(color, 0.2),
+                            '--accent': color,
+                        } as React.CSSProperties
+                    }
+                >
+                    <input
+                        type="color"
+                        className="size-6 rounded-full border-none [&::-moz-color-swatch]:border-none"
+                        value={color}
+                        onChange={(e) => setColor(e.target.value)}
+                    />
+                    <input
+                        style={
+                            {
+                                color: color,
+                                '--accent-placeholder': hexToRGBA(color, 0.45),
+                            } as React.CSSProperties
+                        }
+                        value={newTag}
+                        placeholder={t('title')}
+                        className="focus:outline-none w-28 bg-transparent h-full px-2 placeholder:text-[--accent-placeholder]"
+                        onChange={(e) => setNewTag(e.target.value)}
+                    />
+                    <Button
+                        className="min-w-8 h-full !text-[--accent]"
+                        action={() => {
+                            if (newTag.trim() === '') return
+                            createTag(
+                                { title: newTag.trim(), color: color },
+                                {
+                                    onSuccess: (res) => {
+                                        setColor(initialColor)
+                                        setNewTag('')
+                                    },
+                                }
+                            )
+                        }}
+                    >
+                        <IoAdd size="1.3em" />
+                    </Button>
+                </div>
+                <p className="font-semibold text-sm pb-2 pt-4">
+                    {t('manage_tags')}
+                </p>
+                <ul className="flex flex-wrap gap-2">
+                    {isLoading ? (
+                        <LoadingState.TagPill />
+                    ) : tags?.length ? (
+                        tags?.map((tag) => (
+                            <TaskTag tag={tag} canEdit key={tag.id} />
+                        ))
+                    ) : (
+                        t('no_created_tags')
+                    )}
+                </ul>
+            </div>
+        </SettingsSection>
+    )
+}
+
+export default TeamTags
