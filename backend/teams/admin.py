e23a6f762a0b1d2bec57e4ce7497d4848aa31125
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2025-04-22 20:13:55 +0200

    registered Task, TaskTag, TaskStep in admin

--- backend/teams/admin.py
+++ backend/teams/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Team, Member
+from .models import Team, Member, Task, TaskTag, TaskStep
 
 
 @admin.register(Team)
@@ -15,3 +15,19 @@ class MemberAdmin(admin.ModelAdmin):
     list_display = ("pk", "user", "team", "is_admin")
     list_filter = ("is_admin",)
     empty_value_display = "-empty-"
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "status", "tag", "team")
+    search_fields = ("title",)
+    empty_value_display = "-empty-"
+
+@admin.register(TaskTag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "color")
+    search_fields = ("title",)
+
+@admin.register(TaskStep)
+class StepAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "is_done")
+    search_fields = ("title", "is_done")
