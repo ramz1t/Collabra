3e8f4770379efd3f0a376fd9b31ccaf9a1dec665
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2025-05-31 01:59:56 +0200

    StatusDropdown for task row

---
+++ frontend/src/pages/Team/Tasks/Tasks/List/StatusDropdown.tsx
@@ -0,0 +1,59 @@
+import { Task } from '../../../../../types'
+import cn from 'classnames'
+import { getStatusColor } from '../../../../../utils'
+import React, { useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useUpdateTask } from '../../../../../api/tasks'
+import { useParams } from 'react-router-dom'
+import { Dropdown } from '../../../../../components'
+
+const StatusDropdown = ({ task }: { task: Task }) => {
+    const { t } = useTranslation()
+    const { teamSlug } = useParams()
+    const { mutate: updateTask, isPending } = useUpdateTask(teamSlug!, task.id)
+
+    const handleStatusChange = useCallback(
+        (newStatus: string | null) => {
+            if (newStatus === task.status) return
+            updateTask({ status: newStatus as Task['status'] })
+        },
+        [updateTask, task]
+    )
+
+    return (
+        <Dropdown
+            values={{
+                to_do: 'to_do',
+                in_progress: 'in_progress',
+                need_review: 'need_review',
+                done: 'done',
+            }}
+            renderOption={(status, isSelected) => (
+                <div className="flex items-center justify-between gap-3 w-full">
+                    {t(status)}
+                    <span
+                        className={cn('size-4 rounded-full inline-block')}
+                        style={{
+                            backgroundColor: getStatusColor(status),
+                        }}
+                    ></span>
+                </div>
+            )}
+            renderSelected={(status) => (
+                <div className="flex items-center gap-3">
+                    <span
+                        className={cn('size-4 rounded-full inline-block')}
+                        style={{
+                            backgroundColor: getStatusColor(status),
+                        }}
+                    ></span>
+                    {t(status)}
+                </div>
+            )}
+            selected={task.status}
+            setSelected={handleStatusChange}
+        />
+    )
+}
+
+export default StatusDropdown
