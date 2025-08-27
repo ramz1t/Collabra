4ee5c0d06f43ecb281029e1bfbe53f68dee8b25d
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2025-08-27 14:23:29 +0200

    StepViewSet

---
+++ backend/teams/api/views/steps.py
@@ -0,0 +1,34 @@
+from django.core.exceptions import PermissionDenied
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from django.utils.translation import gettext_lazy as _
+
+from ... import selectors
+from ..serializers import tasks as serializers
+from ...selectors import get_team_or_404,get_task_or_404, get_step_or_404
+from ...services.steps.update import toggle_step
+
+class StepViewSet(viewsets.GenericViewSet):
+    def list(self, request, slug, task_pk):
+        team = selectors.get_team_or_404(slug=slug)
+        task = get_task_or_404(id=task_pk)
+
+        if not selectors.is_user_member_by_team(team, request.user):
+            raise PermissionDenied()
+
+        steps = selectors.get_steps(task=task)
+        serializer = serializers.TaskStepListSerializer(instance=steps, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def toggle(self, request, slug, task_pk, step_pk):
+        team = get_team_or_404(slug=slug)
+        task = get_task_or_404(pk=task_pk)
+        if not (selectors.is_user_admin_by_team(request.user, team)
+                or selectors.is_user_assignee_by_task(request.user, task)):
+            raise PermissionDenied()
+
+        step = get_step_or_404(pk=step_pk)
+        toggle_step(step)
+
+        response_data = {"message": _("Step toggled"), "value": step.is_done}
+        return Response(response_data, status=status.HTTP_200_OK)
